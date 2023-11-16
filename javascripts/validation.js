74e96b5f7be5f18ea31437321397c9dac4cdb7fa
Maria-has <147566488+Maria-has@users.noreply.github.com>
2023-11-16 15:51:13 +0000

    mejoras y correcciones

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -6,22 +6,30 @@ function emailValidation() {
 
   emailConfirmInput.addEventListener('input', () => {
     if (emailInput.value !== emailConfirmInput.value) {
-      emailError.textContent = 'Correo electrónico no coincidente.';
-      emailConfirmInput.classList.add('error'); // Aplica la clase 'error' al campo
+      emailError.textContent = 'Email does not match.';
+      emailError.style.color = '#d14539'; // Cambiar el color del texto a #d14539
+      emailConfirmInput.style.backgroundColor = 'rgba(230, 169, 171, 0.5)'; // Cambiar el color de fondo
+      emailConfirmInput.classList.add('error');
     } else {
       emailError.textContent = '';
-      emailConfirmInput.classList.remove('error'); // Quita la clase 'error' del campo
+      emailError.style.color = ''; // Restaurar el color del texto a su valor predeterminado
+      emailConfirmInput.style.backgroundColor = ''; // Restaurar el color de fondo a su valor predeterminado
+      emailConfirmInput.classList.remove('error');
     }
   });
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (emailInput.value !== emailConfirmInput.value) {
-      emailError.textContent = 'Correo electrónico no coincidente.';
-      emailConfirmInput.classList.add('error'); // Aplica la clase 'error' al campo
+      emailError.textContent = 'Email does not match.';
+      emailError.style.color = '#d14539'; // Cambiar el color del texto a #d14539
+      emailConfirmInput.style.backgroundColor = 'rgba(230, 169, 171, 0.5)'; // Cambiar el color de fondo
+      emailConfirmInput.classList.add('error');
     } else {
       emailError.textContent = '';
-      emailConfirmInput.classList.remove('error'); // Quita la clase 'error' del campo
+      emailError.style.color = ''; // Restaurar el color del texto a su valor predeterminado
+      emailConfirmInput.style.backgroundColor = ''; // Restaurar el color de fondo a su valor predeterminado
+      emailConfirmInput.classList.remove('error');
       form.submit();
     }
   });
