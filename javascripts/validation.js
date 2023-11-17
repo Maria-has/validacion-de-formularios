9aa91b4bc8211852b2c36c18a8b6782f157b5577
Maria-has <147566488+Maria-has@users.noreply.github.com>
2023-11-17 11:25:16 +0000

    cambios

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -7,13 +7,13 @@ function emailValidation() {
   emailConfirmInput.addEventListener('input', () => {
     if (emailInput.value !== emailConfirmInput.value) {
       emailError.textContent = 'Email does not match.';
-      emailError.style.color = '#d14539'; // Cambiar el color del texto a #d14539
-      emailConfirmInput.style.backgroundColor = 'rgba(230, 169, 171, 0.5)'; // Cambiar el color de fondo
+      emailError.style.color = '#d14539'; 
+      emailConfirmInput.style.backgroundColor = 'rgba(230, 169, 171, 0.5)'; 
       emailConfirmInput.classList.add('error');
     } else {
       emailError.textContent = '';
-      emailError.style.color = ''; // Restaurar el color del texto a su valor predeterminado
-      emailConfirmInput.style.backgroundColor = ''; // Restaurar el color de fondo a su valor predeterminado
+      emailError.style.color = ''; 
+      emailConfirmInput.style.backgroundColor = ''; 
       emailConfirmInput.classList.remove('error');
     }
   });
@@ -22,13 +22,13 @@ function emailValidation() {
     e.preventDefault();
     if (emailInput.value !== emailConfirmInput.value) {
       emailError.textContent = 'Email does not match.';
-      emailError.style.color = '#d14539'; // Cambiar el color del texto a #d14539
-      emailConfirmInput.style.backgroundColor = 'rgba(230, 169, 171, 0.5)'; // Cambiar el color de fondo
+      emailError.style.color = '#d14539'; 
+      emailConfirmInput.style.backgroundColor = 'rgba(230, 169, 171, 0.5)'; 
       emailConfirmInput.classList.add('error');
     } else {
       emailError.textContent = '';
-      emailError.style.color = ''; // Restaurar el color del texto a su valor predeterminado
-      emailConfirmInput.style.backgroundColor = ''; // Restaurar el color de fondo a su valor predeterminado
+      emailError.style.color = ''; 
+      emailConfirmInput.style.backgroundColor = ''; 
       emailConfirmInput.classList.remove('error');
       form.submit();
     }
